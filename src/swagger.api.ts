0d70738498d127dde388fd04adcae34be09cd4fc
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2020-12-16 11:53:57 +0200

    Up version

--- src/swagger.api.ts
+++ src/swagger.api.ts
@@ -2,7 +2,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 const options = new DocumentBuilder()
   .setTitle('Veles Services Node.js API docs')
-  .setVersion('2.0.1')
+  .setVersion('3.0.0')
   .addServer('/api')
   .addTag('users')
   .addTag('auth')
