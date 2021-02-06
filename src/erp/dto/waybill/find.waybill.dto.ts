5652d71b3a72c6b26a4226b877a412ee8b878d8e
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-02-06 15:55:08 +0200

    Searching by waybill serial number

--- src/erp/dto/waybill/find.waybill.dto.ts
+++ src/erp/dto/waybill/find.waybill.dto.ts
@@ -1,7 +1,13 @@
-import { IsString, IsOptional } from 'class-validator';
+import { IsString, IsOptional, IsNumber } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class FindWaybillDto {
   @IsString()
   @IsOptional()
   readonly _id?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Transform(({ value }) => Number(value))
+  readonly serialNumber: number;
 }
